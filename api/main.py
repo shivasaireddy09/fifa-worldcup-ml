fee85b9e12b95cfc02d567256656b991dba59c7a
shivasai09 <your-email@example.com>
2026-07-14 11:34:12 +0000

    Fix CORS

--- api/main.py
+++ api/main.py
@@ -22,11 +22,13 @@ app = FastAPI(
     description="Predict FIFA World cup match results using Machine Learning ",
     version="1.0.0"
 )
-app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173", "https://fifa-Worldcup-ml-predictor.vercel.app"],
-                   allow_credentials=True,
-                   allow_methods=["*"],
-                   allow_headers=["*"],
-                   )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model = joblib.load("models/fifa_model.pkl")
 encoder = joblib.load("models/encoder.pkl")
