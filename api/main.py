0ebb140075239818bb368e83026492f3b7fab3a0
shivasai09 <your-email@example.com>
2026-07-14 10:51:08 +0000

    fix cors for vercel

--- api/main.py
+++ api/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
     description="Predict FIFA World cup match results using Machine Learning ",
     version="1.0.0"
 )
-app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173"],
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173", "https://fifa-Worldcup-ml-predictor.vercel.app"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"],
